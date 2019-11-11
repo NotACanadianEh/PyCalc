5ddca2df8b71561ead579ddac6e6b38872c0b7e1
Ryan Jimenez <notacanadianeh@gmail.com>
2019-11-11 18:32:09 +0900

    Added multiplication and division

--- PyCalc.py
+++ PyCalc.py
@@ -5,25 +5,35 @@
 #Addition
 def add(x, y):
     return x + y
-
 #Subtraction
 def subtract(x, y):
     return x - y
+#Division
+def division(x, y):
+    return x / y
+#Multiplication
+def multiplication(x, y):
+    return x * y
 
-print ("Select addition or subtraction")
+#Begins usable part
+print ("Select 1, 2, 3, 4")
 print ("1 Addition")
 print ("2 Subtraction")
-
+print ("3 Division")
+print ("4 Multiplication")
 #Uses 1 or 2 
-choice = input("Select addition or subtraction, 1 or 2 ")
+choice = input("Select one, 1,2,3,4 ")
 number1 = float(input("Choose first number "))
 number2 = float(input ("Choose second number "))
 
 if choice == '1':
     print (number1,"+",number2,"=", add(number1,number2))
-
 elif choice == '2':
     print (number1,"-",number2,"=", subtract(number1,number2))
+elif choice == '3':
+    print (number1,"/",number2,"=", division(number1,number2))
+elif choice == '4':
+    print (number1,"*",number2,"=", multiplication(number1,number2))
 else:
     print ("You dun fucked up A-Aron")
 
